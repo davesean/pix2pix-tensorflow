2a4f8d016132fbb951e3afe1eda4b0f15b45e141
davesean <haldimann.d@gmail.com>
2018-10-31 18:26:06 +0100

    combined the data framework with this implementation of pix2pix for tensorflow

--- model.py
+++ model.py
@@ -7,14 +7,22 @@ import numpy as np
 from six.moves import xrange
 
 from ops import *
-from utils import *
+
+def preprocess(image):
+    with tf.name_scope("preprocess"):
+        # [0, 1] => [-1, 1]
+        return image/255 * 2 - 1
+def deprocess(image):
+    with tf.name_scope("deprocess"):
+        # [-1, 1] => [0, 1]
+        return ((image + 1) / 2)*255
 
 class pix2pix(object):
     def __init__(self, sess, image_size=256,
                  batch_size=1, sample_size=1, output_size=256,
                  gf_dim=64, df_dim=64, L1_lambda=100,
-                 input_c_dim=3, output_c_dim=3, dataset_name='facades',
-                 checkpoint_dir=None, sample_dir=None):
+                 input_c_dim=3, output_c_dim=3, dataset_name='cityscapes_GAN',
+                 checkpoint_dir=None, data=None):
         """
 
         Args:
@@ -62,18 +70,28 @@ class pix2pix(object):
         self.g_bn_d6 = batch_norm(name='g_bn_d6')
         self.g_bn_d7 = batch_norm(name='g_bn_d7')
 
+        self.data = data
         self.dataset_name = dataset_name
         self.checkpoint_dir = checkpoint_dir
-        self.build_model()
 
-    def build_model(self):
-        self.real_data = tf.placeholder(tf.float32,
-                                        [self.batch_size, self.image_size, self.image_size,
-                                         self.input_c_dim + self.output_c_dim],
-                                        name='real_A_and_B_images')
+        # Get the data descriptors with the shape of data coming
+        data_description = data.get_data_description()
+        data_description = [data_description[0], {
+            key: [None, *description]
+            for key, description in data_description[1].items()}]
 
-        self.real_B = self.real_data[:, :, :, :self.input_c_dim]
-        self.real_A = self.real_data[:, :, :, self.input_c_dim:self.input_c_dim + self.output_c_dim]
+        # Create an iterator for the data
+        self.iter_handle = tf.placeholder(tf.string, shape=[],
+                                              name='training_placeholder')
+        iterator = tf.data.Iterator.from_string_handle(
+            self.iter_handle, *data_description)
+        training_batch = iterator.get_next()
+
+        self.build_model(training_batch['labels'], training_batch['rgb'])
+
+    def build_model(self, input, target):
+        self.real_B = preprocess(target)
+        self.real_A = preprocess(input)
 
         self.fake_B = self.generator(self.real_A)
 
@@ -82,21 +100,24 @@ class pix2pix(object):
         self.D, self.D_logits = self.discriminator(self.real_AB, reuse=False)
         self.D_, self.D_logits_ = self.discriminator(self.fake_AB, reuse=True)
 
-        self.fake_B_sample = self.sampler(self.real_A)
-
-        self.d_sum = tf.summary.histogram("d", self.D)
-        self.d__sum = tf.summary.histogram("d_", self.D_)
-        self.fake_B_sum = tf.summary.image("fake_B", self.fake_B)
+        # self.fake_B_sample = self.sampler(self.real_A)
 
         self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits, labels=tf.ones_like(self.D)))
         self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits_, labels=tf.zeros_like(self.D_)))
         self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits_, labels=tf.ones_like(self.D_))) \
                         + self.L1_lambda * tf.reduce_mean(tf.abs(self.real_B - self.fake_B))
 
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+
+        self.d_sum = tf.summary.histogram("d", self.D)
+        self.d__sum = tf.summary.histogram("d_", self.D_)
+        self.real_A_sum = tf.summary.image("target_A", deprocess(self.real_A)[...,::-1])
+        self.real_B_sum = tf.summary.image("real_B", deprocess(self.real_B)[...,::-1])
+        self.fake_B_sum = tf.summary.image("fake_B", deprocess(self.fake_B)[...,::-1])
+
         self.d_loss_real_sum = tf.summary.scalar("d_loss_real", self.d_loss_real)
         self.d_loss_fake_sum = tf.summary.scalar("d_loss_fake", self.d_loss_fake)
-
-        self.d_loss = self.d_loss_real + self.d_loss_fake
 
         self.g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
         self.d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
@@ -108,27 +129,6 @@ class pix2pix(object):
 
         self.saver = tf.train.Saver()
 
-
-    def load_random_samples(self):
-        data = np.random.choice(glob('./datasets/{}/val/*.jpg'.format(self.dataset_name)), self.batch_size)
-        sample = [load_data(sample_file) for sample_file in data]
-
-        if (self.is_grayscale):
-            sample_images = np.array(sample).astype(np.float32)[:, :, :, None]
-        else:
-            sample_images = np.array(sample).astype(np.float32)
-        return sample_images
-
-    def sample_model(self, sample_dir, epoch, idx):
-        sample_images = self.load_random_samples()
-        samples, d_loss, g_loss = self.sess.run(
-            [self.fake_B_sample, self.d_loss, self.g_loss],
-            feed_dict={self.real_data: sample_images}
-        )
-        save_images(samples, [self.batch_size, 1],
-                    './{}/train_{:02d}_{:04d}.png'.format(sample_dir, epoch, idx))
-        print("[Sample] d_loss: {:.8f}, g_loss: {:.8f}".format(d_loss, g_loss))
-
     def train(self, args):
         """Train pix2pix"""
         d_optim = tf.train.AdamOptimizer(args.lr, beta1=args.beta1) \
@@ -139,10 +139,10 @@ class pix2pix(object):
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
 
-        self.g_sum = tf.summary.merge([self.d__sum,
+        self.g_sum = tf.summary.merge([self.d__sum,self.real_A_sum,self.real_B_sum,
             self.fake_B_sum, self.d_loss_fake_sum, self.g_loss_sum])
         self.d_sum = tf.summary.merge([self.d_sum, self.d_loss_real_sum, self.d_loss_sum])
-        self.writer = tf.summary.FileWriter("./logs", self.sess.graph)
+        self.writer = tf.summary.FileWriter(self.checkpoint_dir, self.sess.graph)
 
         counter = 1
         start_time = time.time()
@@ -152,48 +152,66 @@ class pix2pix(object):
         else:
             print(" [!] Load failed...")
 
-        for epoch in xrange(args.epoch):
-            data = glob('./datasets/{}/train/*.jpg'.format(self.dataset_name))
-            #np.random.shuffle(data)
-            batch_idxs = min(len(data), args.train_size) // self.batch_size
+        # for epoch in xrange(args.epoch):
+        for epoch in range(0,1):
+            input_data = self.data.get_trainset()
+            data_iterator = input_data.repeat(args.max_epochs).batch(args.batch_size).make_one_shot_iterator()
+            data_handle = self.sess.run(data_iterator.string_handle())
 
-            for idx in xrange(0, batch_idxs):
-                batch_files = data[idx*self.batch_size:(idx+1)*self.batch_size]
-                batch = [load_data(batch_file) for batch_file in batch_files]
-                if (self.is_grayscale):
-                    batch_images = np.array(batch).astype(np.float32)[:, :, :, None]
-                else:
-                    batch_images = np.array(batch).astype(np.float32)
+            # for idx in xrange(0, batch_idxs):
+            while True:
 
                 # Update D network
-                _, summary_str = self.sess.run([d_optim, self.d_sum],
-                                               feed_dict={ self.real_data: batch_images })
-                self.writer.add_summary(summary_str, counter)
+                try:
+                    _, summary_str = self.sess.run([d_optim, self.d_sum],
+                                                   feed_dict={ self.iter_handle: data_handle })
+                    self.writer.add_summary(summary_str, counter)
+                except tf.errors.OutOfRangeError:
+                    print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                    break
 
                 # Update G network
-                _, summary_str = self.sess.run([g_optim, self.g_sum],
-                                               feed_dict={ self.real_data: batch_images })
-                self.writer.add_summary(summary_str, counter)
+                try:
+                    _, summary_str = self.sess.run([g_optim, self.g_sum],
+                                                   feed_dict={ self.iter_handle: data_handle })
+                    self.writer.add_summary(summary_str, counter)
+                except tf.errors.OutOfRangeError:
+                    print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                    break
 
                 # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                _, summary_str = self.sess.run([g_optim, self.g_sum],
-                                               feed_dict={ self.real_data: batch_images })
-                self.writer.add_summary(summary_str, counter)
-
-                errD_fake = self.d_loss_fake.eval({self.real_data: batch_images})
-                errD_real = self.d_loss_real.eval({self.real_data: batch_images})
-                errG = self.g_loss.eval({self.real_data: batch_images})
+                # try:
+                #     _, summary_str = self.sess.run([g_optim, self.g_sum],
+                #                                    feed_dict={ self.iter_handle: data_handle })
+                #     self.writer.add_summary(summary_str, counter)
+                # except tf.errors.OutOfRangeError:
+                #     print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                #     break
+                try:
+                    errD_fake = self.d_loss_fake.eval({self.iter_handle: data_handle})
+                except tf.errors.OutOfRangeError:
+                    print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                    break
+                try:
+                    errD_real = self.d_loss_real.eval({self.iter_handle: data_handle})
+                except tf.errors.OutOfRangeError:
+                    print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                    break
+                try:
+                    errG = self.g_loss.eval({self.iter_handle: data_handle})
+                except tf.errors.OutOfRangeError:
+                    print("INFO: Training set has %d elements, starting next epoch" % counter-1)
+                    break
 
                 counter += 1
-                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
-                    % (epoch, idx, batch_idxs,
-                        time.time() - start_time, errD_fake+errD_real, errG))
+                print("Step: [%2d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                    % (counter,time.time() - start_time, errD_fake+errD_real, errG))
 
-                if np.mod(counter, 100) == 1:
-                    self.sample_model(args.sample_dir, epoch, idx)
+                # if np.mod(counter, 100) == 1:
+                #     self.sample_model(args.sample_dir, epoch, idx)
 
                 if np.mod(counter, 500) == 2:
-                    self.save(args.checkpoint_dir, counter)
+                    self.save(self.checkpoint_dir, counter)
 
     def discriminator(self, image, y=None, reuse=False):
 
@@ -366,20 +384,20 @@ class pix2pix(object):
     def save(self, checkpoint_dir, step):
         model_name = "pix2pix.model"
         model_dir = "%s_%s_%s" % (self.dataset_name, self.batch_size, self.output_size)
-        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+        checkpoint_dir = os.path.join(self.checkpoint_dir, model_dir)
 
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
 
         self.saver.save(self.sess,
-                        os.path.join(checkpoint_dir, model_name),
+                        os.path.join(self.checkpoint_dir, model_name),
                         global_step=step)
 
     def load(self, checkpoint_dir):
         print(" [*] Reading checkpoint...")
 
         model_dir = "%s_%s_%s" % (self.dataset_name, self.batch_size, self.output_size)
-        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+        checkpoint_dir = os.path.join(self.checkpoint_dir, model_dir)
 
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
@@ -389,43 +407,43 @@ class pix2pix(object):
         else:
             return False
 
-    def test(self, args):
-        """Test pix2pix"""
-        init_op = tf.global_variables_initializer()
-        self.sess.run(init_op)
-
-        sample_files = glob('./datasets/{}/val/*.jpg'.format(self.dataset_name))
-
-        # sort testing input
-        n = [int(i) for i in map(lambda x: x.split('/')[-1].split('.jpg')[0], sample_files)]
-        sample_files = [x for (y, x) in sorted(zip(n, sample_files))]
-
-        # load testing input
-        print("Loading testing images ...")
-        sample = [load_data(sample_file, is_test=True) for sample_file in sample_files]
-
-        if (self.is_grayscale):
-            sample_images = np.array(sample).astype(np.float32)[:, :, :, None]
-        else:
-            sample_images = np.array(sample).astype(np.float32)
-
-        sample_images = [sample_images[i:i+self.batch_size]
-                         for i in xrange(0, len(sample_images), self.batch_size)]
-        sample_images = np.array(sample_images)
-        print(sample_images.shape)
-
-        start_time = time.time()
-        if self.load(self.checkpoint_dir):
-            print(" [*] Load SUCCESS")
-        else:
-            print(" [!] Load failed...")
-
-        for i, sample_image in enumerate(sample_images):
-            idx = i+1
-            print("sampling image ", idx)
-            samples = self.sess.run(
-                self.fake_B_sample,
-                feed_dict={self.real_data: sample_image}
-            )
-            save_images(samples, [self.batch_size, 1],
-                        './{}/test_{:04d}.png'.format(args.test_dir, idx))
+    # def test(self, args):
+    #     """Test pix2pix"""
+    #     init_op = tf.global_variables_initializer()
+    #     self.sess.run(init_op)
+    #
+    #     sample_files = glob('./datasets/{}/val/*.jpg'.format(self.dataset_name))
+    #
+    #     # sort testing input
+    #     n = [int(i) for i in map(lambda x: x.split('/')[-1].split('.jpg')[0], sample_files)]
+    #     sample_files = [x for (y, x) in sorted(zip(n, sample_files))]
+    #
+    #     # load testing input
+    #     print("Loading testing images ...")
+    #     sample = [load_data(sample_file, is_test=True) for sample_file in sample_files]
+    #
+    #     if (self.is_grayscale):
+    #         sample_images = np.array(sample).astype(np.float32)[:, :, :, None]
+    #     else:
+    #         sample_images = np.array(sample).astype(np.float32)
+    #
+    #     sample_images = [sample_images[i:i+self.batch_size]
+    #                      for i in xrange(0, len(sample_images), self.batch_size)]
+    #     sample_images = np.array(sample_images)
+    #     print(sample_images.shape)
+    #
+    #     start_time = time.time()
+    #     if self.load(self.checkpoint_dir):
+    #         print(" [*] Load SUCCESS")
+    #     else:
+    #         print(" [!] Load failed...")
+    #
+    #     for i, sample_image in enumerate(sample_images):
+    #         idx = i+1
+    #         print("sampling image ", idx)
+    #         samples = self.sess.run(
+    #             self.fake_B_sample,
+    #             feed_dict={self.real_data: sample_image}
+    #         )
+    #         save_images(samples, [self.batch_size, 1],
+    #                     './{}/test_{:04d}.png'.format(args.test_dir, idx))
--- ops.py
+++ ops.py
@@ -1,10 +1,8 @@
 import math
-import numpy as np 
+import numpy as np
 import tensorflow as tf
 
 from tensorflow.python.framework import ops
-
-from utils import *
 
 class batch_norm(object):
             # h1 = lrelu(tf.contrib.layers.batch_norm(conv2d(h0, self.df_dim*2, name='d_h1_conv'),decay=0.9,updates_collections=None,epsilon=0.00001,scale=True,scope="d_h1_conv"))
@@ -41,7 +39,7 @@ def conv_cond_concat(x, y):
     y_shapes = y.get_shape()
     return tf.concat([x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
 
-def conv2d(input_, output_dim, 
+def conv2d(input_, output_dim,
            k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
            name="conv2d"):
     with tf.variable_scope(name):
@@ -50,7 +48,7 @@ def conv2d(input_, output_dim,
         conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
 
         biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-        conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+        conv = tf.reshape(tf.nn.bias_add(conv, biases), tf.shape(conv))
 
         return conv
 
@@ -61,7 +59,7 @@ def deconv2d(input_, output_shape,
         # filter : [height, width, output_channels, in_channels]
         w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
                             initializer=tf.random_normal_initializer(stddev=stddev))
-        
+
         try:
             deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
                                 strides=[1, d_h, d_w, 1])
@@ -78,16 +76,16 @@ def deconv2d(input_, output_shape,
             return deconv, w, biases
         else:
             return deconv
-       
+
 
 def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
 
 def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
-    shape = input_.get_shape().as_list()
-
+    # shape = input_.get_shape().as_list()
+    shape = 16 * 16 * 64 * 8 * 4
     with tf.variable_scope(scope or "Linear"):
-        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+        matrix = tf.get_variable("Matrix", [shape, output_size], tf.float32,
                                  tf.random_normal_initializer(stddev=stddev))
         bias = tf.get_variable("bias", [output_size],
             initializer=tf.constant_initializer(bias_start))
